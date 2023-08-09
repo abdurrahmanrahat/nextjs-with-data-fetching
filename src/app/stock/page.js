e0327d30fada109148fe430a326c344babf0b5db
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-08-09 18:02:58 +0600

    implement a table design in stock page

--- src/app/stock/page.js
+++ src/app/stock/page.js
@@ -20,7 +20,9 @@ const StockPage = () => {
           {/* Stock product Name Field */}
           <div className="form-control">
             <label className="label">
-              <span className="label-text font-semibold">Stock product name*</span>
+              <span className="label-text font-semibold">
+                Stock product name*
+              </span>
             </label>
             <input
               type="text"
@@ -79,6 +81,44 @@ const StockPage = () => {
           />
         </div>
       </form>
+
+      {/* Table for displaying stock products */}
+      <div class="flex justify-center my-12">
+        <div class="w-full lg:w-10/12">
+          <h2 className="text-3xl text-center font-semibold my-8">
+            Show Stock Products
+          </h2>
+          <div class="bg-white shadow-md rounded-lg overflow-hidden">
+            <table class="w-full">
+              <thead>
+                <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                  <th class="py-3 px-6 text-left">ID</th>
+                  <th class="py-3 px-6 text-left">Name</th>
+                  <th class="py-3 px-6 text-center">Age</th>
+                  <th class="py-3 px-6 text-center">Country</th>
+                  <th class="py-3 px-6 text-center">Actions</th>
+                </tr>
+              </thead>
+              <tbody class="text-gray-600 text-sm font-light">
+                <tr class="border-b border-gray-200 hover:bg-gray-100">
+                  <td class="py-3 px-6 text-left">1</td>
+                  <td class="py-3 px-6 text-left">John Doe</td>
+                  <td class="py-3 px-6 text-center">28</td>
+                  <td class="py-3 px-6 text-center">USA</td>
+                  <td class="py-3 px-6 text-center">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                      Edit
+                    </button>
+                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 ml-2 rounded-full">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
