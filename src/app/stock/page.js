4d238c71d8d053984ac8767598f728455b23c128
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-08-09 21:28:48 +0600

    fetching data and see in the table

--- src/app/stock/page.js
+++ src/app/stock/page.js
@@ -1,11 +1,22 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 
 const StockPage = () => {
+  const [stocks, setStocks] = useState([]);
+
   const { register, handleSubmit, reset } = useForm();
 
+  // post request for getting data from db
+  useEffect(() => {
+    fetch("/api/product")
+      .then((res) => res.json())
+      .then((data) => setStocks(data));
+  }, []);
+
+  // form onsubmit function
   const onSubmit = async (data) => {
     console.log(data);
     const { name, email, quantity, price } = data;
@@ -103,34 +114,39 @@ const StockPage = () => {
       <div className="flex justify-center my-12">
         <div className="w-full lg:w-10/12">
           <h2 className="text-3xl text-center font-semibold my-8">
-            Show Stock Products
+            Show Stock Products: {stocks?.length}
           </h2>
           <div className="bg-white shadow-md rounded-lg overflow-hidden">
             <table className="w-full">
               <thead>
                 <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                  <th className="py-3 px-6 text-left">ID</th>
-                  <th className="py-3 px-6 text-left">Name</th>
-                  <th className="py-3 px-6 text-center">Age</th>
-                  <th className="py-3 px-6 text-center">Country</th>
+                  <th className="py-3 px-6 text-left">#</th>
+                  <th className="py-3 px-6 text-left">Stock Name</th>
+                  <th className="py-3 px-6 text-center">Quantity</th>
+                  <th className="py-3 px-6 text-center">Price</th>
                   <th className="py-3 px-6 text-center">Actions</th>
                 </tr>
               </thead>
               <tbody className="text-gray-600 text-sm font-light">
-                <tr className="border-b border-gray-200 hover:bg-gray-100">
-                  <td className="py-3 px-6 text-left">1</td>
-                  <td className="py-3 px-6 text-left">John Doe</td>
-                  <td className="py-3 px-6 text-center">28</td>
-                  <td className="py-3 px-6 text-center">USA</td>
-                  <td className="py-3 px-6 text-center">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                      Edit
-                    </button>
-                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 ml-2 rounded-full">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
+                {stocks.map((stock, i) => (
+                  <tr
+                    key={stock.id}
+                    className="border-b border-gray-200 hover:bg-gray-100"
+                  >
+                    <td className="py-3 px-6 text-left">{i + 1}</td>
+                    <td className="py-3 px-6 text-left">{stock.name}</td>
+                    <td className="py-3 px-6 text-center">{stock.quantity}</td>
+                    <td className="py-3 px-6 text-center">${stock.price}</td>
+                    <td className="py-3 px-6 text-center">
+                      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                        Edit
+                      </button>
+                      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 ml-2 rounded-full">
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
